eb53581228a199fb4c8babf4f283cbb60d8e03e6
WenLiu197 <wen-liuqingyang@outlook.com>
2026-08-16 11:52:10 +0800

    feat: 创建主机

--- src/CeruleanToolkit.Cli/Program.cs
+++ src/CeruleanToolkit.Cli/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace CeruleanToolkit.Cli
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.CommandLine;
+
+var builder = Host.CreateApplicationBuilder();
+// TODO: 后续配置 DI 与日志过滤
+using IHost host = builder.Build();
+
+try
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    await host.StartAsync();
+
+
+}
+finally
+{
+    await host.StopAsync();
 }
